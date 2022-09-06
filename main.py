eda7acc68609feabf5c9d050fd32065b838f3dc1
김민재 <kmj94102@naver.com>
2022-09-06 19:46:49 +0900

    test

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
-from cgitb import reset
-from inspect import Attribute
-import logging
-from operator import attrgetter, index
-from unittest import result
 
 from fastapi import FastAPI
 from typing import List
 from starlette.middleware.cors import CORSMiddleware
-from sqlalchemy.orm import aliased
-from itertools import combinations
-
-from db import session
-from model import PokemonTable, Pokemon, EvolutionTable, Evolution, EvolutionTypeTable, EvolutionType, CharacteristicTable, Characteristic, SearchInfo
-
 app = FastAPI()
 
 app.add_middleware(
